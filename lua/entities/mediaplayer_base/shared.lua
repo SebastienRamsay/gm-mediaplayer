ce0d8e54c61ab684411df706402127ea70924266
Sebastien Ramsay <sebastien.ramsay@gmail.com>
2026-02-26 21:40:20 -0500

    yea InitializeDestructible is a server only function...

--- lua/entities/mediaplayer_base/shared.lua
+++ lua/entities/mediaplayer_base/shared.lua
@@ -32,23 +32,23 @@ function ENT:Initialize()
 
 		-- Network media player ID
 		self:SetMediaPlayerID( mp:GetId() )
+
+		timer.Simple(0, function()
+			if not IsValid(self) then return end
+			if self.CPPIGetOwner and self:CPPIGetOwner() and self:CPPIGetOwner():IsPlayer() then
+				-- Attach mixin methods to self
+				for k, v in pairs(DestructibleMixin) do self[k] = v end
+				self.HealthAmount = RPrint.HpModifiers[self:GetClass()] or 1500
+				self.CanCatchFire = true
+				self.CanStartSparking = true
+				self.ExplodeOnDestruct = true
+				self:InitializeDestructible()
+			end
+		end)
 	end
 
 	-- Apply player config based on model
 	self.PlayerConfig = self:GetMediaPlayerConfig()
-
-	timer.Simple(0, function()
-		if not IsValid(self) then return end
-		if self.CPPIGetOwner and self:CPPIGetOwner() and self:CPPIGetOwner():IsPlayer() then
-			-- Attach mixin methods to self
-			for k, v in pairs(DestructibleMixin) do self[k] = v end
-			self.HealthAmount = RPrint.HpModifiers[self:GetClass()] or 1500
-			self.CanCatchFire = true
-			self.CanStartSparking = true
-			self.ExplodeOnDestruct = true
-			self:InitializeDestructible()
-		end
-	end)
 end
 
 function ENT:SetupDataTables()
