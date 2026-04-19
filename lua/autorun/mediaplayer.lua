bdff59e9c12adaba714cfb42fba36fd1c19dd470
Sebastien Ramsay <sebastien.ramsay@gmail.com>
2026-04-19 19:46:16 -0400

    removed resource.AddWorkshop

--- lua/autorun/mediaplayer.lua
+++ lua/autorun/mediaplayer.lua
@@ -58,7 +58,7 @@ local function LoadMediaPlayer()
 
 	if SERVER then
 		-- Add mediaplayer models
-		resource.AddWorkshop( "3001397905" )
+		-- resource.AddWorkshop( "3001397905" )
 
 		-- download clientside includes
 		AddCSLuaFile "includes/modules/browserpool.lua"
